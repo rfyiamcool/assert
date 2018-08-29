6216dc7c9d297c78f3b38d3d8a22423ad11858b0
rfyiamcool <rfyiamcool@163.com>
2018-08-30 07:41:47 +0800

    assert

---
+++ assert.go
@@ -0,0 +1,74 @@
+package assert
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var repos = map[reflect.Type][]cmp.Option{}
+
+func Add(v interface{}, opts ...cmp.Option) {
+	if v == nil || len(opts) == 0 {
+		return
+	}
+	typ, ok := v.(reflect.Type)
+	if !ok {
+		typ = reflect.TypeOf(v)
+	}
+	curr, ok := repos[typ]
+	if !ok {
+		curr = make([]cmp.Option, 0, len(opts))
+	}
+	repos[typ] = append(curr, opts...)
+}
+
+func get(typ reflect.Type) []cmp.Option {
+	if typ == nil {
+		return nil
+	}
+	return repos[typ]
+}
+
+type logger interface {
+	Helper()
+	Logf(format string, args ...interface{})
+}
+
+func check(l logger, x, y interface{}) bool {
+	l.Helper()
+
+	tx, ty := reflect.TypeOf(x), reflect.TypeOf(y)
+	opts := get(tx)
+	if tx != ty {
+		opts2 := get(ty)
+		if len(opts2) > 0 {
+			tmp := make([]cmp.Option, 0, len(opts)+len(opts2))
+			opts = append(append(tmp, opts...), opts2...)
+		}
+	}
+
+	diff := cmp.Diff(x, y, opts...)
+	if diff == "" {
+		return true
+	}
+	l.Logf("not equal\n%s", diff)
+	return false
+}
+
+func Equal(tb testing.TB, x, y interface{}) {
+	tb.Helper()
+	if check(tb, x, y) {
+		return
+	}
+	tb.Fail()
+}
+
+func FatalEqual(tb testing.TB, x, y interface{}) {
+	tb.Helper()
+	if check(tb, x, y) {
+		return
+	}
+	tb.FailNow()
+}
